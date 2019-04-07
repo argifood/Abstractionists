f9d39c03a7e7c1faf11815898bbb3349badda345
lampros18 <44374754+lampros18@users.noreply.github.com>
2019-04-07 15:35:20 +0300

    Final and stable version

--- arduino.c
+++ arduino.c
@@ -1,35 +1,33 @@
 #include <SoftwareSerial.h>
 //#include "Timer.h"
- 
+
 //Timer t;
 SoftwareSerial SIM900A(2,3); // RX | TX
 char c = ' ';
 bool canLoop = false;
 unsigned long time_now = 0;
-void setup()
+void setup() 
 {
-/*  pinMode(4,OUTPUT);
+  pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
-l
+
   digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);*/
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
- 
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  
   Serial.begin(9600);
   while(!Serial);
   //Starting the communication
-  SIM900A.begin(9600);
+  SIM900A.begin(9600); 
   Serial.println("Please wait...");
- 
+
   String response = "";
- 
+  
   delay(500);
   response = callCommand("AT+SAPBR=0,1"); //Closing any previous connections
   delay(500);
- 
+
   response = callCommand("AT");
   if(response.equals("ERROR")){
     Serial.println("Something went wrong, restart");
@@ -76,107 +74,141 @@ l
     }
   }
 }
- 
 void loop()
 {
- time_now = millis();
+ time_now = millis(); 
   while(millis() < time_now + 200){
-     
+      
   }
   callHttpCommand("AT+HTTPACTION=0");
   while(SIM900A.available()){
     c = SIM900A.read();
   }
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 200){
-     
+      
   }
   callCommand("AT+HTTPREAD");
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 200){
-     
+      
   }
   while(SIM900A.available()){
     c = SIM900A.read();
   }
- 
-  callCommand("AT+HTTPREAD");
-  while(SIM900A.available()){
-    c = SIM900A.read();
-  }
-  time_now = millis();
-  while(millis() < time_now + 200){
-     
-  }
-  while(SIM900A.available()){
-    c = SIM900A.read();
-  }
+  
   callCommand("AT+HTTPREAD");
   while(SIM900A.available()){
     c = SIM900A.read();
   }
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 200){
-     
+      
+  }
+  while(SIM900A.available()){
+    c = SIM900A.read();
+  }
+  callCommand("AT+HTTPREAD");
+  while(SIM900A.available()){
+    c = SIM900A.read();
+  }
+  time_now = millis(); 
+  while(millis() < time_now + 200){
+      
   }
   while(SIM900A.available()){
     c = SIM900A.read();
   }
   //
   String temp = callRequestCommand("AT+HTTPREAD");
-  Serial.println(temp);
+
+  char str[temp.length()+1];
+  temp.toCharArray(str, temp.length()+1);
+
+    String values[3][2];
+    int keysCounter = 0;
+    values[0][0] = "";
+    values[1][0] = "";
+    values[2][0] = "";
+    values[0][1] = "";
+    values[1][1] = "";
+    values[2][1] = "";
+  
+    // Returns first token 
+    char* token = strtok(str, ","); 
+    char* temp2;
+    while (token != NULL) { 
+       // Serial.println(token);
+        int valuesCounter = 0;
+        String temp2 = token;
+        for(int i = 0; i<temp2.length()+1; i++){
+          if(temp2.charAt(i) != '-'){
+            values[keysCounter][valuesCounter] += temp2.charAt(i);
+          }
+          if(temp2.charAt(i) == '-'){
+            valuesCounter++;
+          }
+        }
+        keysCounter++;
+        token = strtok(NULL, ","); 
+    }
+ //   Serial.println(values[1][1]);
+    digitalWrite(values[0][0].toInt(), values[0][1]=="L"?LOW:HIGH);
+    digitalWrite(values[1][0].toInt(), values[1][1]=="L"?LOW:HIGH);
+    digitalWrite(values[2][0].toInt(), values[2][1]=="L"?LOW:HIGH);
+    //Serial.println(values[1][1]);
 }
- 
+
 void getPumps(){
   Serial.println("STARTING");
   String response = "";
   callCommand("AT+HTTPPARA=\"URL\",\"abstractionists.alwaysdata.net/api/getPumps.php\"");
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 500){
-     
+      
   }
   while(SIM900A.available()){
     c = SIM900A.read();
   }
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 500){
-     
+      
   }
   callHttpCommand("AT+HTTPACTION=0");
   while(SIM900A.available()){
     c = SIM900A.read();
   }
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 150){
-     
+      
   }
   callCommand("AT+HTTPREAD");
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 150){
-     
+      
   }
   while(SIM900A.available()){
     c = SIM900A.read();
   }
- 
-  callCommand("AT+HTTPREAD");
-  while(SIM900A.available()){
-    c = SIM900A.read();
-  }
-  time_now = millis();
-  while(millis() < time_now + 150){
-     
-  }
-  while(SIM900A.available()){
-    c = SIM900A.read();
-  }
+  
   callCommand("AT+HTTPREAD");
   while(SIM900A.available()){
     c = SIM900A.read();
   }
-  time_now = millis();
+  time_now = millis(); 
   while(millis() < time_now + 150){
-     
+      
+  }
+  while(SIM900A.available()){
+    c = SIM900A.read();
+  }
+  callCommand("AT+HTTPREAD");
+  while(SIM900A.available()){
+    c = SIM900A.read();
+  }
+  time_now = millis(); 
+  while(millis() < time_now + 150){
+      
   }
   while(SIM900A.available()){
     c = SIM900A.read();
@@ -184,15 +216,15 @@ void getPumps(){
   //
   String temp = callRequestCommand("AT+HTTPREAD");
   Serial.println(temp);
-  loopPumps();
+  canLoop=true;
 }
- 
+
 String callCommand(String cmd){
   String tmp = "";
   String response = "";
   boolean canContinue = false;
   int counter = 0; //If it reaches 5, there was definitely something wrong and we should stop
- 
+
   //Calling the command until it works (sometimes some commands say ERROR the first time)
   while(!canContinue){
     cmd += "\r\n";
@@ -217,11 +249,11 @@ String callCommand(String cmd){
         }
       }
     }
-   
+    
     //Converting to byte array to use strtok
     //The response has 3 or more lines. The first line is our command, and the last is OK/ERROR
     //The lines in between are the response
-   
+    
     char *ptr = NULL;
     char charBuf[tmp.length() + 1];
     tmp.toCharArray(charBuf, tmp.length()+1);
@@ -236,19 +268,19 @@ String callCommand(String cmd){
       ptr = strtok(NULL, "\n");
     }
     for(int n = 1; n < index-1; n++) //First and last lines are the ones we don't need
-   {
+   { 
     response += strings[n];
    }
    return response;
   }
 }
- 
+
 String callHttpCommand(String cmd){
   String tmp = "";
   String response = "";
   boolean canContinue = false;
   int counter = 0; //If it reaches 5, there was definitely something wrong and we should stop
- 
+
   //Calling the command until it works (sometimes some commands say ERROR the first time)
   while(!canContinue){
     cmd += "\r\n";
@@ -273,11 +305,11 @@ String callHttpCommand(String cmd){
         }
       }
     }
-   
+    
     //Converting to byte array to use strtok
     //The response has 3 or more lines. The first line is our command, and the last is OK/ERROR
     //The lines in between are the response
-   
+    
     char *ptr = NULL;
     char charBuf[tmp.length() + 1];
     tmp.toCharArray(charBuf, tmp.length()+1);
@@ -292,7 +324,7 @@ String callHttpCommand(String cmd){
       ptr = strtok(NULL, "\n");
     }
     for(int n = 3; n < index-1; n++) //First and last lines are the ones we don't need
-   {
+   { 
     response += strings[n];
    }
    return response;
@@ -303,7 +335,7 @@ String callRequestCommand(String cmd){
   String response = "";
   boolean canContinue = false;
   int counter = 0; //If it reaches 5, there was definitely something wrong and we should stop
- 
+
   //Calling the command until it works (sometimes some commands say ERROR the first time)
   while(!canContinue){
     cmd += "\r\n";
@@ -328,11 +360,11 @@ String callRequestCommand(String cmd){
         }
       }
     }
-   
+    
     //Converting to byte array to use strtok
     //The response has 3 or more lines. The first line is our command, and the last is OK/ERROR
     //The lines in between are the response
-   
+    
     char *ptr = NULL;
     char charBuf[tmp.length() + 1];
     tmp.toCharArray(charBuf, tmp.length()+1);
@@ -347,7 +379,7 @@ String callRequestCommand(String cmd){
       ptr = strtok(NULL, "\n");
     }
     for(int n = 3; n < index-1; n++) //First and last lines are the ones we don't need
-   {
+   { 
     response += strings[n];
    }
    response.trim();
